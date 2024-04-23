caabf43dc9d9e5bb503740db07a7385cb86bf700
aditya bhojane <adityabhojane66@gmail.com>
2024-04-24 00:34:36 +0530

    add count down

--- main.js
+++ main.js
@@ -41,3 +41,39 @@ bubble.addEventListener("click",()=>{
         cardVisible = true
     }
 })
+
+/******************************* */
+// countdown
+let Days = 47;
+let hrs = 6;
+let mins =  57;
+let sec = 8;
+
+setInterval(()=>{   
+    sec--;
+    if(hrs <= 0){
+        Days--;
+        hrs = 24;
+    }if(mins <= 0){
+        hrs--
+        mins = 60;
+    }if(sec <= 0){
+        mins--
+        sec = 60;
+    }
+ 
+    document.getElementById("days").innerHTML = Days;
+    document.getElementById("hrs").innerHTML = hrs;
+    document.getElementById("mins").innerHTML = mins;
+    document.getElementById("secs").innerHTML = sec;
+    if(Days == 0 && hrs == 0 && mins == 0 && sec == 0){
+        document.getElementById("days").innerHTML = 0;
+        document.getElementById("hrs").innerHTML = 0;
+        document.getElementById("mins").innerHTML = 0;
+        document.getElementById("sec").innerHTML = 0;
+    }
+    
+},1000)
+
+/******************************* */
+
